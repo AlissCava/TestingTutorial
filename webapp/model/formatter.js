f777a5896857371017c723db9193478dc21b1ba8
AlissCava <cavalleraalissa@gmail.com>
2026-02-09 10:31:58 +0100

    Step 2: A First Unit Test

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,5 @@
 sap.ui.define([], function () {
 	"use strict";
-
 	return {
 		/**
 		 * Rounds the number unit value to 2 digits
@@ -13,10 +12,9 @@ sap.ui.define([], function () {
 			if (!sValue) {
 				return "";
 			}
-
 			return parseFloat(sValue).toFixed(2);
+		},
+		priceState: function () {
 		}
-
 	};
-
 });
