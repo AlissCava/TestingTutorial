e6bac7ecd68496914e034347dc0b726e9e75ffdc
AlissCava <cavalleraalissa@gmail.com>
2026-02-11 10:39:09 +0100

    Step 13: Testing User Interaction

--- webapp/test/integration/pages/Post.js
+++ webapp/test/integration/pages/Post.js
@@ -3,8 +3,10 @@ sap.ui.define([
 	'sap/ui/test/matchers/Properties',
 	'sap/ui/test/actions/Press'
 ], function (Opa5, Properties, Press) {
-		"use strict";
+	"use strict";
+
 		var sViewName = "Post";
+
 		Opa5.createPageObjects({
 			onThePostPage: {
 				actions: {
@@ -15,27 +17,46 @@ sap.ui.define([
 							actions: new Press(),
 							errorMessage: "Did not find the nav button on object page"
 						});
+					},
+					iPressOnTheTabWithTheKey: function (sKey) {
+						return this.waitFor({
+							controlType: "sap.m.IconTabFilter",
+							viewName : sViewName,
+							matchers: new Properties({
+								key: sKey
+							}),
+							actions: new Press(),
+							errorMessage: "Cannot find the icon tab bar"
+						});
 					}
 				},
 				assertions: {
 					theTitleShouldDisplayTheName: function (sName) {
 						return this.waitFor({
+							id: "objectHeader",
+							viewName: sViewName,
+							matchers: new Properties({
+								title: sName
+							}),
+							success: function (oPage) {
+								Opa5.assert.ok(true, "was on the remembered detail page");
+							},
+							errorMessage: "The Post " + sName + " is not shown"
+						});
+					},
+
+					iShouldSeeTheViewCounter: function () {
+						return this.waitFor({
+							id: "viewCounter",
+							viewName: sViewName,
 							success: function () {
-								return this.waitFor({
-									id: "objectHeader",
-									viewName: sViewName,
-									matchers: new Properties({
-										title: sName
-									}),
-									success: function (oPage) {
-										Opa5.assert.ok(true, "was on the remembered detail page");
-									},
-									errorMessage: "The Post " + sName + " is not shown"
-								});
-							}
+								Opa5.assert.ok(true, "The view counter was visible");
+							},
+							errorMessage: "The view counter could not be found"
 						});
 					}
 				}
 			}
-		});
-	});
+		}); 
+	}
+);
